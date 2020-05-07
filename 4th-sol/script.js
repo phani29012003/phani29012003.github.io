d28387839092eb1b4ff44e3c8af74f750b122507
phani29012003 <naga121333@gmail.com>
2020-05-07 12:05:41 +0530

    gp

--- 4th-sol/script.js
+++ 4th-sol/script.js
@@ -1,11 +1,17 @@
 var arr=["Yaakov","John", "Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
+function helloSpeaker(nam){
+	console.log("Hello "+nam);
+}
+function byeSpeaker(nam){
+	console.log("Good Bye "+nam);
+}
 for(var name in arr){
 	var a=Array.from(arr[name]);
 	if(a[0]==="j"||a[0]=="J"){
-		console.log("Good Bye "+arr[name]);
+		byeSpeaker(arr[name]);
 	}
 	else{
-		console.log("Hello "+arr[name]);
+		helloSpeaker(arr[name]);
 	}
 }
 
