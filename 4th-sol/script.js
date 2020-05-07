5eb830daad6ad60182c592cd1c25251a28579eb3
phani29012003 <naga121333@gmail.com>
2020-05-07 11:54:09 +0530

    initial Commit

--- 4th-sol/script.js
+++ 4th-sol/script.js
@@ -1,8 +1,8 @@
-var arr=["Satya","Jashwanth","Jaipal","Karunakar","Jahnavi"];
+var arr=["Yaakov","John", "Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
 for(var name in arr){
 	var a=Array.from(arr[name]);
 	if(a[0]==="j"||a[0]=="J"){
-		console.log("Goodbye "+arr[name]);
+		console.log("Good Bye "+arr[name]);
 	}
 	else{
 		console.log("Hello "+arr[name]);
